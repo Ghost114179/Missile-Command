e309d5d6d412fe993cb54bb4b89cb338d054e215
Ghost114179 <eric114179@gmail.com>
2016-12-22 21:52:33 -0600

    New City Models Implemented
    Implemented new models that each city can spawn with for a more
    interesting look.

--- Assets/cityBehavior.cs
+++ Assets/cityBehavior.cs
@@ -2,10 +2,33 @@
 using System.Collections;
 
 public class cityBehavior : MonoBehaviour {
+	public MeshRenderer FutureCityMesh;
+	public MeshRenderer RuralTownMesh;
+	public MeshRenderer ModernCityMesh;
 
 	// Use this for initialization
 	void Start () {
-
+		FutureCityMesh.enabled = false;
+		RuralTownMesh.enabled = false;
+		ModernCityMesh.enabled = false;
+		switch (Random.Range (0, 3)) {
+		case 0:
+			//print ("Future");
+			FutureCityMesh.enabled = true;
+			break;
+		case 1:
+			//print ("Rural");
+			RuralTownMesh.enabled = true;
+			break;
+		case 2:
+			//print ("Modern");
+			ModernCityMesh.enabled = true;
+			break;
+		default:
+			//print ("Default");
+			FutureCityMesh.enabled = true;
+			break;
+		}
 	}
 	
 	// Update is called once per frame
